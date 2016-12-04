21ce4caffec6d4ba5cddc988c95e9ec95d9588e0
Motoki Shiga <shiga_m@gifu-u.ac.jp>
2016-12-04 19:05:43 +0900

    Add files via upload

---
+++ demo_output_DecisionTree_graph.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn import datasets
+from sklearn import tree
+
+# load dataset IRIS
+iris = datasets.load_iris()
+
+# training
+classifier = tree.DecisionTreeClassifier()
+classifier = classifier.fit(iris.data, iris.target)
+
+# output decision tree
+with open('dt_graph.dot', 'w') as file_out:
+    file_out = tree.export_graphviz(classifier, out_file=file_out)
+
+#### To output into a pdf file, run a command by Graphviz
+# > dot -Tpdf dt_graph.dot -o dt_graph.pdf 
